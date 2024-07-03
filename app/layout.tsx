264f76d8f499131142c1939f31da92b9ed25b983
Victor Ribeiro <contato.victoribeiro@gmail.com>
2024-07-03 17:11:32 -0300

    ClerkSetup

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { ConvexClientProvider } from "./providers/ConvexProvider";
+import  ConvexClerkProvider  from "./providers/ConvexClerkProvider";
+
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,7 +22,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <ConvexClientProvider>{children}</ConvexClientProvider>        
+        <ConvexClerkProvider>
+          {children}
+        </ConvexClerkProvider>        
       </body>
     </html>
   );
---
+++ convex/auth.config.ts
@@ -0,0 +1,10 @@
+const authConfig = {
+    providers: [
+        {
+            domain: "https://stunning-midge-31.clerk.accounts.dev",
+            applicationID: "convex",
+        },
+    ]
+};
+
+export default authConfig;
